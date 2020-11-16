b20e361fa328aa7527a64654aa1877b073d64f44
GiuseppeCutrano <giuseppecutrano.info@gmail.com>
2020-11-16 12:52:42 +0100

    fine

---
+++ libs/main.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+    el: "#root",
+    data: {
+        greeting: "Hello Vue",
+        codeImg: "https://images.unsplash.com/photo-1576836165612-8bc9b07e7778?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+    }
+
+});
